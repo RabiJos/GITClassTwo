de011aede4a2880ca26abd715668805836589400
RabiJos <rabijos96@gmail.com>
2021-11-13 22:15:45 +0530

    GIT Class

---
+++ src/test/java/org/prg/two/Class0ne.java
@@ -0,0 +1,16 @@
+package org.prg.two;
+
+public class Class0ne {
+private void method1() {
+	// TODO Auto-generated method stub
+
+}
+private void method2() {
+	// TODO Auto-generated method stub
+
+}
+private void method3() {
+	// TODO Auto-generated method stub
+
+}
+}
